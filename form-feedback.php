66812f8f64369bb1d336ad2bac9975ca96e37cca
Francisco Bizi <taylorsoft28@gmail.com>
2022-04-01 13:59:04 +0300

    Plugin structure

---
+++ form-feedback.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Form Feedback  
+Description: Small form feedback management plugin
+Version: 1.0.0
+Author: Francisco Bizi
+Author Email: dev@gmail.com
+License: MIT
+*/
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+define('PLUGIN_FBP_DIR', plugin_dir_path(__FILE__));
+define('PLUGIN_FBP_URL', plugin_dir_url(__FILE__));
+require_once("vendor/autoload.php");
+
+use FBP\Core;
+
+/**
+ *    Create table on activation hook event
+ */
+register_activation_hook(__FILE__, [(new Core()), 'fbf_install']);
+
+/**
+ *    Drop table on unnstall hook event
+ */
+register_uninstall_hook(__FILE__, [(new Core()), 'fbf_uninstall']);
---
+++ src/Core.php
@@ -0,0 +1,64 @@
+<?php
+namespace FBP;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * 
+ * Core class that manage the plugin
+ * 
+ * @author Francisco Bizi <dev@support.com>
+ * 
+ */
+final class Core
+{
+
+    function __construct()
+    {
+        
+        $this->init();
+        
+	}
+    
+	private function init()
+	{
+        $this->load_on_init();
+
+		add_action('wp_enqueue_scripts',			[$this, '_fbp_enquee_scripts']);
+        
+        //ajax 
+        add_action('wp_ajax_fbp_requests',          [$this, '_fbp_ajax_requests']);
+        add_action('wp_ajax_nopriv_fbp_requests',   [$this, '_fbp_ajax_requests']);
+    }
+
+    public function load_on_init()
+    {
+        require_once dirname(__FILE__) . "/inc/FBF_Admin_Menu.php";
+        require_once dirname(__FILE__) . "/inc/FBF_Tables.php";
+        require_once dirname(__FILE__) . "/shortcodes/form-shortcode.php";
+    }
+
+    public function _fbp_ajax_requests()
+    {
+        require_once dirname(__FILE__)."/inc/ajax.php";
+    }
+
+    public function _fbp_enquee_scripts()
+    {
+        wp_enqueue_style("fbp-style", plugins_url('/src/assets/css/styles.css', dirname(__FILE__)) ,false, time(), 'all');
+        wp_enqueue_script("fbp-js", plugins_url('/src/assets/js/script.js', dirname(__FILE__)), array('jquery'), time(), true);
+        wp_localize_script('fbp-js', 'ajax_var', 
+                                array(
+                                        'endpoint' => admin_url('admin-ajax.php'),
+                                    ));
+    }
+    
+    function fbf_install()
+    {
+    }
+
+    function fbf_uninstall()
+    {
+    }
+    
+}
